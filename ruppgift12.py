9804432027970620ed12e4c50eac64e5e496514d
Linnea Karlström <linnea.karlstrom@skola.botkyrka.se>
2019-12-16 10:08:19 +0100

    work in progress

--- ruppgift12.py
+++ ruppgift12.py
@@ -1,10 +1,2 @@
-import random
+from ruppgift9 import divisble_by
 
-def divisble_by(number):
-    for i in range(1,1000):
-        if number % 7 == 0:
-            return True
-        else:
-            return False
-
-print(divisble_by(random.randrange(1,1000)))
